d0117ad3d1ff9fb38bf05d3909d0bc7dfe6f64bb
Elvis Estevez <124850509+ElvisAbner@users.noreply.github.com>
2023-09-06 16:17:11 -0400

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is a Note Title</h1>
+      <p>This a Note Paragraph</p>
+    </div>
+  );
+}
+
+export default Note;
